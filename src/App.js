6a924e63fd903eada2ec42d976aa69c244ab3590
Coysin <6672788@gmail.com>
2022-12-16 20:53:33 +0300

    111

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
 function App() {
+
+  React.useEffect(()=>{
+
+    axios.get(process.env.REACT_APP_API)
+
+    
+    //такая запись равна ('http://localhost:3000/cart')
+    // axios.get(process.env.REACT_APP_API+'/cart')
+    
+
+
+  },[])
+
+  const array=[
+    {
+      type:'shell',
+      value: process.env.REACT_APP_SHELL || 'нет данных'
+    },
+    {
+      name:'env',
+      value: process.env.REACT_APP_ENV_FILE || 'нет данных env'
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +44,24 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        {
+          array.map(({type, value}, index)=>(
+            <div>
+              <p>
+                <b>Index: {index+1}</b>
+              </p>
+              <p>
+                <b>Type: {type}</b>
+              </p>
+              <p>
+                <b>Value: {value}</b>
+              </p>
+            </div>
+          ))
+        }
+      </div>
+
     </div>
   );
 }
